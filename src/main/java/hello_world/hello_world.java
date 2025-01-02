e694e80927d0d2c6957a5cb1762484a89db89c79
ranma15g <ranma15g@gmail.com>
2025-01-01 18:18:06 -0800

    Initial Commit

---
+++ src/main/java/hello_world/hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hello_world;
+
+/**
+ *
+ * @author ranma
+ */
+public class hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Fuck You");
+    }
+}
